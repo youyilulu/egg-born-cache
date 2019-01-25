aabf68c59cdf6bf82d81b5a2601ab7d81715931e
youyilulu <83722442@qq.com>
2019-01-25 14:36:43 +0800

    change cache strategy

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,4 @@
 'use strict';
 
-const CACHE_STRATEGY_REDIS = Symbol.for('EggBorn#CacheStrategy#redis');
-
 module.exports = () => {
-  const config = Object.create(null);
-  config.cacheStrategy = CACHE_STRATEGY_REDIS;
-  config.redis = {
-    host: '127.0.0.1',
-    port: 6379,
-  };
-  return config;
 };
--- test/fixtures/apps/redisapp-test/config/config.default.js
+++ test/fixtures/apps/redisapp-test/config/config.default.js
@@ -1,14 +1,7 @@
 'use strict';
 
-const CACHE_STRATEGY_REDIS = Symbol.for('EggBorn#CacheStrategy#redis');
-
 module.exports = () => {
   const config = {
-    cacheStrategy: CACHE_STRATEGY_REDIS,
-    redis: {
-      host: '127.0.0.1',
-      port: 6379,
-    },
     keys: 'cache_20190124',
   };
   return config;
